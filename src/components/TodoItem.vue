e9bbb753f61d56501b8881196b6d4775b38b1dbd
FabioMiguelJr <74741157+FabioMiguelJr@users.noreply.github.com>
2022-08-01 16:35:40 -0300

    Commit - Conclusão do desafio guiado 2

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <InputText v-model="description" />
+  <div class="div-botoes">
+    <ButtonVue @click="cancel" class="btn-voltar">Voltar</ButtonVue>
+    <ButtonVue @click="saveTodo">Salvar</ButtonVue>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      description: "",
+    };
+  },
+  props: {
+    todo: Object,
+  },
+  created() {
+    //console.log(this.todo);
+    if (this.todo) {
+      this.description = this.todo.description;
+    }
+  },
+  methods: {
+    cancel() {
+      this.$emit("cancel");
+    },
+    saveTodo() {
+      this.$emit("saveTodo", { ...this.todo, description: this.description });
+    },
+  },
+};
+</script>
+<style scoped>
+.div-botoes {
+  margin-top: 1rem;
+}
+.btn-voltar {
+  margin-right: 1rem;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="todo-list">
+    <div class="todo-item" v-for="(todo, index) in todos" :key="index">
+      <label class="todo-label">
+        {{ todo.description }}
+      </label>
+      <ButtonVue
+        icon="pi pi-pencil"
+        class="p-button-rounded p-button-text"
+        v-on:click="$emit('editTodo', index)"
+      ></ButtonVue>
+      <ButtonVue
+        icon="pi pi-trash"
+        class="p-button-rounded p-button-text"
+        v-on:click="$emit('deleteTodo', index)"
+      ></ButtonVue>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    todos: Array,
+  },
+};
+</script>
+
+<style scoped>
+.todo-list {
+  margin: 1rem;
+}
+.todo-item {
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid lightgray;
+}
+.todo-label {
+  flex-grow: 1;
+}
+</style>
